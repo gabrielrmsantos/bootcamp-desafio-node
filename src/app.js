d32fdf1c8b75d1be6783e59acc2bb4dd52d762c5
Gabriel Roberto <gabrielrmsantos@outlook.com>
2020-04-13 00:14:19 -0300

    working routes and middlewares

--- src/app.js
+++ src/app.js
@@ -1,33 +1,75 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
+
+const repositoryExists = (request, response, next) => {
+  const id = request.params.id;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0)
+    return response.status(400).json({ message: 'Repository not exists!' });
+
+  request.body.repositoryIndex = repositoryIndex;
+  return next();
+}
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 
+app.use('/repositories/:id', repositoryExists);
+
+// routes
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+  return response.status(201).json(repositories);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const { title, url, techs, repositoryIndex } = request.body;
+  const repository = {
+    id,
+    title: title || repositories[repositoryIndex].title,
+    url: url || repositories[repositoryIndex].url,
+    techs: techs || repositories[repositoryIndex].techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository;
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repositoryIndex = request.body.repositoryIndex;
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repositoryIndex = request.body.repositoryIndex;
+  repositories[repositoryIndex].likes++;
+
+  return response.status(201).json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
